0fe27c321be3f9baeb51fc692cd6d5b25e5d078a
Noah V <noah@noahvandenberg.com>
2024-10-31 02:30:14 -0700

    Initial Commit

---
+++ Progress Battery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Progress Battery
+//
+//  Created by Noah Vandenberg on 2024-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Progress Battery/Progress_BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_BatteryApp.swift
+//  Progress Battery
+//
+//  Created by Noah Vandenberg on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct Progress_BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
